b5171f4158777b084d26a170b3741a108d2b8160
unknown <silachunyong@gmail.com>
2025-07-17 12:31:49 +0700

    first commit

---
+++ app/routes/Captain-lab02.tsx
@@ -0,0 +1,14 @@
+function MyProfile(){
+ return (
+<div>
+     <p className="text-2xl">Captain Profile
+     <img src ="/images/mum.png.jpg" />
+     <p className="text-lg"/>
+        Name : Sila Chunyong <br />
+        Student ID:026730491004-0<br />
+        </p>
+    </div>
+ )
+}
+
+export default MyProfile;
